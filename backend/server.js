35ee142d9b34331b24efd968d8b3f739978081ff
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-10-09 11:15:58 +0000

    Implement user login and sign up feature.
    created the user router, the user controller and the user model.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const workoutRouter = require('./routes/workouts');
+// register routes
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/users');
 
 
 // express app
@@ -20,7 +22,8 @@ app.use((req, res, next) => {
 });
   
 // route handlers
-app.use('/api/workouts', workoutRouter);
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI ||'mongodb://localhost/workout')
